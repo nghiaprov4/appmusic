63c839d1ee0c285ceec7cbe29afeadd93b909ae0
DESKTOP-41VA80C\IBM <thieuquangnghia2000@gmail.com>
2021-10-07 18:38:54 +0700

    thieuquangnghia_18041651

--- app/src/main/java/com/example/appmusic/MainActivity.java
+++ app/src/main/java/com/example/appmusic/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
